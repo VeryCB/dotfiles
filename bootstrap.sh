44acd76a14b02ca89ad4500df2a4809ed08c6979
VeryCB <imcaibin@gmail.com>
2021-06-26 01:05:20 +1200

    use single square brackets

--- bootstrap.sh
+++ bootstrap.sh
@@ -34,7 +34,7 @@ do
 done
 
 echo "Installing Vim plugins..."
-if [[ ! -d "$HOME/.vim/bundle/Vundle.vim" ]]; then
+if [ ! -d "$HOME/.vim/bundle/Vundle.vim" ]; then
   git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
 fi
 vim +PluginInstall +qall
