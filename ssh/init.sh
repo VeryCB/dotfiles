bdb484287d143b3d7bfa9d6071f87ff9360604e5
VeryCB <imcaibin@gmail.com>
2021-10-18 00:48:26 +1300

    update script

--- ssh/init.sh
+++ ssh/init.sh
@@ -10,6 +10,6 @@ ssh-keygen -t ed25519 -C $2 -f ~/.ssh/id_ed25519_shepeth
 eval "$(ssh-agent -s)"
 ssh-add -K ~/.ssh/id_ed25519_shepeth
 
-cp config ~/.ssh
+cp ./ssh/config ~/.ssh
 
 echo "run 'pbcopy < ~/.ssh/id_ed25519_verycb.pub' and 'pbcopy < ~/.ssh/id_ed25519_shepeth.pub', paste them into seperate GitHub accounts"
